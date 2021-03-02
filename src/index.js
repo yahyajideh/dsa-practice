b0a0209c327ff69106ef643a5cb2c91a4cadc4eb
yahyajideh <yahya.jideh@gmail.com>
2021-03-02 23:23:49 +1100

    Problem 1108. Defanging an IP Address

--- src/index.js
+++ src/index.js
@@ -3,3 +3,4 @@ import "regenerator-runtime/runtime";
 import * as leetcode from "./leetcode/solutions.js";
 
 console.log(leetcode.runningSum([1, 2, 3, 4]));
+console.log(leetcode.defangIPaddr("1.1.1.1"));
--- src/leetcode/solutions.js
+++ src/leetcode/solutions.js
@@ -1,5 +1,5 @@
 /**
- * Problem 1480 - Running Sum of 1d Array
+ * Problem 1480. Running Sum of 1d Array
  * @link https://leetcode.com/problems/running-sum-of-1d-array/
  * @param {number[]} nums
  * @return {number[]}
@@ -7,8 +7,17 @@
  */
 const runningSum = function (nums) {
   let count = 0;
-  const runningSum = nums.map((el) => (count += el));
+  const runningSum = nums.map(el => (count += el));
   return runningSum;
 };
 
-export { runningSum };
+/**
+ * Problem 1108. Defanging an IP Address
+ * @param {string} address
+ * @return {string}
+ */
+const defangIPaddr = function (address) {
+  return address.replace(/\./g, '[.]');
+};
+
+export { runningSum, defangIPaddr };
